d4cb8f2ac1e7cd204750e85342783a84b5a26dc4
Lihaha <liyang@chromxhealth.com>
2023-10-24 18:09:28 +0800

    test

---
+++ firewater/firewater.h
@@ -0,0 +1,19 @@
+﻿#ifndef FIREWATER_H
+#define FIREWATER_H
+
+#include "dataengineinterface.h"
+
+class FireWater : public QObject, public DataEngineInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(DataEngineInterface)
+    Q_PLUGIN_METADATA(IID "VOFA+.Plugin.FireWater")
+
+public:
+    explicit FireWater();
+    ~FireWater();
+    void ProcessingDatas(char *data, int count);
+private:
+    uint32_t image_count_mutation_count_ = 0;
+};
+#endif // FIREWATER_H
---
+++ fluewater/fluewater.h
@@ -0,0 +1,19 @@
+﻿#ifndef FLUEWATER_H
+#define FLUEWATER_H
+
+#include "dataengineinterface.h"
+
+class FlueWater : public QObject, public DataEngineInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(DataEngineInterface)
+    Q_PLUGIN_METADATA(IID "VOFA+.Plugin.FlueWater")
+
+public:
+    explicit FlueWater();
+    ~FlueWater();
+    void ProcessingDatas(char *data, int count);
+private:
+    uint32_t image_count_mutation_count_ = 0;
+};
+#endif // FLUEWATER_H
